dd56c92af2e509e76f58feb201a11455b7a77a12
Sonia Gorla <soniagorla@msn.com>
2019-05-12 15:57:07 -0500

    Add login feature, change add color/category design

--- views.py
+++ views.py
@@ -1,11 +1,23 @@
-from models import Base, User, Category, Color
+from oauth2client.client import FlowExchangeError
+from oauth2client.client import flow_from_clientsecrets
+import httplib2
+import json
+import requests
+import string
+import random
 from flask import Flask, render_template, jsonify, request, url_for, abort, g, redirect, flash
+from flask import session as login_session
+from flask import make_response
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
+from models import Base, User, Category, Color
 
 from flask_httpauth import HTTPBasicAuth
 auth = HTTPBasicAuth()
+
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())[
+    'web']['client_id']
 
 engine = create_engine('sqlite:///colors.db',
                        connect_args={'check_same_thread': False})
@@ -14,6 +26,186 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 app = Flask(__name__)
+
+# Create a state token to prevent request forgery.
+# Store it in the session for later validation.
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    # If a user is logged in, get current user info
+    current_user = {}
+    if 'username' in login_session:
+        getCurrentUserInfo(current_user)
+    return render_template('login.html', STATE=state, CLIENT_ID=CLIENT_ID, current_user=current_user)
+
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def createUser(login_session):
+    newUser = User(name=login_session['username'],
+                   email=login_session['email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getCurrentUserInfo(current_user):
+    # Get all current user's info
+    current_user['username'] = login_session['username']
+    current_user['picture'] = login_session['picture']
+    current_user['email'] = login_session['email']
+    return current_user
+
+# GConnect
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+  # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'appplication/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(json.dumps(
+            'Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = (
+        'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # if there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access is used for the indended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(json.dumps(
+            "Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(json.dumps(
+            "Token's client ID does not match app's"), 401)
+        print("Token's client ID does not match app's")
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check to see if user is already logged in
+    stored_credentials = login_session.get('credentials')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps(
+            'Current user is already connected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+    data = json.loads(answer.text)
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    # see if user exists, if it doesn't, make a new one
+    user_id = getUserID(data['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+        login_session['user_id'] = user_id
+        print('New User Created!')
+    else:
+        print('User Already Exists')
+        # login_session['user_id'] = user_id
+        # user_info = getUserInfo(user_id)
+        # print('User Found!')
+    output = ''
+    output += '<p>Welcome, '
+    output += login_session['username']
+    output += '!</p>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 200px; height: 200px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print("Login completed")
+    return output
+# End of gconnect
+
+
+# DISCONNECT - Revoke a current user's token and reset their login_session.
+@app.route("/gdisconnect")
+def gdisconnect():
+  # Only disconnect a connected user.
+  # Check if a user is connected.
+    try:
+        access_token = login_session['access_token']
+    except KeyError:
+        access_token = None
+    if access_token is None:
+        response = make_response(json.dumps(
+            'Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Execute HTTP GET request to revoke current token.
+    url = "https://accounts.google.com/o/oauth2/revoke?token=%s" % access_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+
+    if result['status'] == '200':
+        # Reset the user's session.
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+
+        # response = make_response(json.dumps('Successfully disconnected.'), 200)
+        # response.headers['Content-Type'] = 'application/json'
+        # return response
+        flash('Successfully disconnected')
+        return redirect(url_for("showLogin"))
+    else:
+        # For whatever reason, the given token was invalid.
+        response = make_response(json.dumps(
+            'Failed to revoke token for given user.'), 400)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
 
 # API for categories
 @app.route('/categories/JSON')
@@ -36,29 +228,44 @@ def showCategories():
         colors = session.query(Color).filter_by(
             category_id=category.id).all()
         category.all_colors = colors
-    return render_template('categories.html', categories=categories)
+    # If a user is logged in, get current user info
+    current_user = {}
+    if 'username' in login_session:
+        getCurrentUserInfo(current_user)
+    return render_template('categories.html', categories=categories, current_user=current_user)
 
 # Show all colors belonging to a category
 @app.route('/category/<int:category_id>/', methods=['GET', 'POST'])
 def showColors(category_id):
     category = session.query(Category).filter_by(id=category_id).one()
-    # creator = getUserInfo(category.user_id)
     colors = session.query(Color).filter_by(
         category_id=category_id).all()
-    # if 'username' not in login_session or creator.id != login_session['user_id']:
-    #     return render_template('publiclist.html', items=items, category=category, creator=creator)
-    # else:
-    #     return render_template('publiclist.html', items=items, category=category, creator=creator)
-    return render_template('colorsPerCategory.html', category=category, colors=colors)
+    # get info about the user, if logged in
+    creator = getUserInfo(category.user_id)
+    print('Category created by %s, current user = %s' %
+          (category.user_id, login_session['user_id']))
+   # If a user is logged in, get current user info
+    current_user = {}
+    if 'username' in login_session:
+        getCurrentUserInfo(current_user)
+    # If a user is not logged in, or the user logged in isn't the creator, return the public page
+    if 'username' not in login_session or creator.id != login_session['user_id']:
+        return render_template('colorsPerCategoryPublic.html', colors=colors, category=category, creator=creator, current_user=current_user)
+        # Else, return a page where s/he can fully edit the category
+    else:
+        return render_template('colorsPerCategory.html', category=category, colors=colors, creator=creator)
 
 # Create a new category
 @app.route('/category/new/', methods=['GET', 'POST'])
 def newCategory():
-    # if 'username' not in login_session:
-    #     return redirect('/login')
+    # If a user isn't logged in, it isn't possible to make a new category,
+    # so redirect to login page
+    if 'username' not in login_session:
+        return redirect('/login')
+    # If a request is sent, add a new category
     if request.method == 'POST':
         newCategory = Category(
-            name=request.form['name'], user_id=request.form['user_id'])
+            name=request.form['name'], user_id=login_session['user_id'])
         session.add(newCategory)
         flash('New Category "%s" Successfully Created' % newCategory.name)
         session.commit()
@@ -118,10 +325,10 @@ def newColor(category_id):
 # Edit a color
 @app.route('/category/<int:category_id>/color/<int:color_id>/edit', methods=['GET', 'POST'])
 def editColor(category_id, color_id):
-    # if 'username' not in login_session:
-    #     return redirect('/login')
+    if 'username' not in login_session:
+        return redirect('/login')
     editedColor = session.query(Color).filter_by(id=color_id).one()
-    category = session.query(Category).filter_by(id=category_id).one()
+    # category = session.query(Category).filter_by(id=category_id).one()
     if request.method == 'POST':
         if request.form['name']:
             editedColor.name = request.form['name']
@@ -145,8 +352,11 @@ def editColor(category_id, color_id):
 # Delete a color
 @app.route('/category/<int:category_id>/color/<int:color_id>/delete', methods=['GET', 'POST'])
 def deleteColor(category_id, color_id):
-    # if 'username' not in login_session:
-    #     return redirect('/login')
+    # Chceck if a user is logged in
+    if 'username' not in login_session:
+        return redirect('/login')
+    else:
+        print(login_session)
     # category = session.query(Category).filter_by(id=category_id).one()
     colorToDelete = session.query(Color).filter_by(id=color_id).one()
     if request.method == 'POST':
@@ -161,5 +371,4 @@ def deleteColor(category_id, color_id):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    #app.config['SECRET_KEY'] = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
     app.run(host='0.0.0.0', port=8000)
